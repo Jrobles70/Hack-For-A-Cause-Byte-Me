202d2541755f63d60509344f4f9d90a854935c32
bearwithscissors <wyatt@wyattreed.com>
2018-04-06 22:11:31 -0700

    basic flask

---
+++ source/flask_main.py
@@ -0,0 +1,101 @@
+"""
+Flask web app connects to Mongo database.
+Keep a simple list of dated memoranda.
+
+Representation conventions for dates:
+   - We use Arrow objects when we want to manipulate dates, but for all
+     storage in database, in session or g objects, or anything else that
+     needs a text representation, we use ISO date strings.  These sort in the
+     order as arrow date objects, and they are easy to convert to and from
+     arrow date objects.  (For display on screen, we use the 'humanize' filter
+     below.) A time zone offset will
+   - User input/output is in local (to the server) time.
+"""
+
+import flask
+from flask import g
+from flask import render_template
+from flask import request
+from flask import url_for
+
+import json
+import logging
+
+# Date handling
+import arrow    # Replacement for datetime, based on moment.js
+# import datetime # But we may still need time
+from dateutil import tz  # For interpreting local times
+
+# Mongo database
+from pymongo import MongoClient
+import pymongo
+# for use removing _ids
+from bson.objectid import ObjectId
+import secrets.admin_secrets
+import secrets.client_secrets
+MONGO_CLIENT_URL = "mongodb://{}:{}@localhost:{}/{}".format(
+    secrets.client_secrets.db_user,
+    secrets.client_secrets.db_user_pw,
+    secrets.admin_secrets.port,
+    secrets.client_secrets.db)
+
+###
+# Globals
+###
+import CONFIG
+app = flask.Flask(__name__)
+app.secret_key = CONFIG.secret_key
+
+####
+# Database connection per server process
+###
+
+try:
+    dbclient = MongoClient(MONGO_CLIENT_URL)
+    db = getattr(dbclient, secrets.client_secrets.db)
+    collection = db.dated
+
+except:
+    print("Failure opening database.  Is Mongo running? Correct password?")
+    sys.exit(1)
+
+
+
+###
+# Pages
+###
+
+@app.route("/")
+@app.route("/index")
+def index():
+  app.logger.debug("Main page entry")
+  g.memos = get_memos()
+  for memo in g.memos:
+      app.logger.debug("Memo: " + str(memo))
+  return flask.render_template('index.html')
+
+
+# We don't have an interface for creating memos yet
+@app.route("/create")
+def create():
+    app.logger.debug("Create")
+    return flask.render_template('create.html')
+
+
+@app.route("/_example")
+def example():
+
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.debug("Page not found")
+    return flask.render_template('page_not_found.html',
+                                 badurl=request.base_url,
+                                 linkback=url_for("index")), 404
+
+
+if __name__ == "__main__":
+    app.debug=CONFIG.DEBUG
+    app.logger.setLevel(logging.DEBUG)
+    app.run(port=CONFIG.PORT,host="0.0.0.0")
